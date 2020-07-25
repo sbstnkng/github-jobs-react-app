0712424a28bb9fbea34effd0bb90d8b954a27532
sbstnkng <29359422+sbstnkng@users.noreply.github.com>
2020-07-25 07:44:28 +0000

    Deploying to gh-pages from  @ 07f51aeee3eafa1dbc0b0e7393eca524c12f3b5d 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e63776edb09d68ba5ca13ab07ac95859.js"
+  "/github-jobs-react-app/precache-manifest.502610ba82e9f8db1428dd3eeadc1be5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-jobs-react-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
